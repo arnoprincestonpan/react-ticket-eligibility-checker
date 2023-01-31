4367bb0f7c33e79ed2b649610c7f806a6130ecce
arnoprincestonpan <arnopan@Arnos-MBP.lan>
2023-01-31 02:16:21 -0800

    display issues, review proper formatting.

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ function App() {
   const [age, setAge] = useState(0)
   const [quantity, setQuantity] = useState(0)
   const [cash, setCash] = useState(0)
+  const [message, setMessage] = useState("")
 
   let msg = ""
   let ticketCost = 12.99 + 1.14
   let ageRequirement = 16
 
   function checkEligibility(){
-    if((firstName != "" && lastName != "") 
+    if((firstName !== "" && lastName !== "") 
     || (typeof(firstName) == String && typeof(lastName) == String)){
       msg += "<li>" + firstName + " " + lastName + " is a valid name.</li>"
     }else{
       msg += "<li>" + firstName + " " + lastName + " is not a valid name.</li>"
     }
 
-    if(typeof(age) == Number || age != 0){
+    if(typeof(age) == Number || age !== 0){
       if(age >= ageRequirement){
         msg += "<li>" + age + ", is of age to watch this movie.</li>"
       }else{
@@ -30,7 +31,7 @@ function App() {
       msg += "<li>Please enter a valid number.</li>"
     }
 
-    if((typeof(quantity) == Number || quantity != 0) && (typeof(cash) == Number) || cash != 0){
+    if((typeof(quantity) == Number && quantity !== 0) && (typeof(cash) == Number) && cash !== 0){
       if(quantity * ticketCost <= cash){
         msg += "<li>" + cash + "is enough for " + quantity + " tickets.</li>"
       }else{
@@ -45,17 +46,42 @@ function App() {
     }else{
       msg += "<li>Sorry, you couldn't purchase any tickets.</li>"
     }
+
+    setMessage(msg)
   }
 
   return (
     <div className="body">
       <h1>Ticket Eligibility Checker</h1>
       <div>
-        <label for="first-name">First Name: </label>
+        <label htmlFor="first-name">First Name: </label>
         <input onChange={(e) => setFirstName(e.target.value)} id="first-name" name="first-name" type="text"  />
       </div>
+
       <div>
-        {msg}
+        <label htmlFor="last-name">Last Name: </label>
+        <input onChange={(e) => setLastName(e.target.value)} id="last-name" name="last-name" type="text"  />
+      </div>
+
+      <div>
+        <label htmlFor="age">Age: </label>
+        <input onChange={(e) => setAge(e.target.valueAsNumber)} id="age" name="age" type="number"  />
+      </div>
+
+      <div>
+        <label htmlFor="quantity">Quantity: </label>
+        <input onChange={(e) => setQuantity(e.target.valueAsNumber)} id="quantity" name="quantity" type="number"  />
+      </div>
+
+      <div>
+        <label htmlFor="cash">Cash: </label>
+        <input onChange={(e) => setCash(e.target.valueAsNumber)} id="cash" name="cash" type="number"  />
+      </div>
+
+      <input onClick={() => checkEligibility()} id="submit-button" type="submit" value="Check Eligibility"  />
+      
+      <div>
+        {message}
       </div>
     </div>
   );
