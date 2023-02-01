3e164ef3d2e8b0c3d99b955c78f01cf0c2da6bd2
arnoprincestonpan <arnopan@Arnos-MBP.lan>
2023-01-31 17:30:42 -0800

    changed logic and got msg to display

--- src/App.js
+++ src/App.js
@@ -14,36 +14,34 @@ function App() {
   let ageRequirement = 16
 
   function checkEligibility(){
-    if((firstName !== "" && lastName !== "") 
-    || (typeof(firstName) == String && typeof(lastName) == String)){
-      msg += "<li>" + firstName + " " + lastName + " is a valid name.</li>"
+    if(typeof(firstName) === "string"){
+      msg += "<li>" + firstName + " is a valid String.</li>"
     }else{
-      msg += "<li>" + firstName + " " + lastName + " is not a valid name.</li>"
+      msg += "<li>" + firstName + " is not a valid String.</li>"
     }
 
-    if(typeof(age) == Number || age !== 0){
-      if(age >= ageRequirement){
-        msg += "<li>" + age + ", is of age to watch this movie.</li>"
-      }else{
-        msg += "<li>" + age + ", is not of age to watch this movie.</li>"
-      }
+    if(typeof(lastName) === "string"){
+      msg += "<li>" + lastName + " is a valid String.</li>"
     }else{
-      msg += "<li>Please enter a valid number.</li>"
+      msg += "<li>" + lastName + " is not a valid String.</li>"
     }
 
-    if((typeof(quantity) == Number && quantity !== 0) && (typeof(cash) == Number) && cash !== 0){
-      if(quantity * ticketCost <= cash){
-        msg += "<li>" + cash + "is enough for " + quantity + " tickets.</li>"
-      }else{
-        msg += "<li>" + cash + "is not enough for " + quantity + " tickets.</li>"
-      }
+    if(typeof(quantity) === "number" && typeof(cash) === "number" && typeof(age) === "number"){
+      msg += "<li>You have entered valid quantity: " + quantity + ", cash: $" + cash + " and age: " + age + " numbers.</li>"
     }else{
-      msg += "<li>Please check your quantity and cash amount.</li>"
+      msg += "<li>You have not entered valid quantity: " + quantity + ", cash: $" + cash + " and age: " + age + " numbers.</li>"
     }
 
     if(quantity * ticketCost <= cash && age >= ageRequirement){
+      msg += "<li>You have " + cash + ", which is enough to buy " + quantity + " tickets at $" + ticketCost + " each.</li>"
       msg += "<li>Congratulations you bought some ticket(s).</li>"
     }else{
+      if(age <= ageRequirement){
+        msg += "<li>" + age + ", is not of age for a " + ageRequirement +  " age requirement.</li>"
+      }
+      if(quantity * ticketCost >= cash){
+        msg += "<li>" + cash + " is not enough to buy " + quantity + " of " + ticketCost + " tickets.</li>"
+      }
       msg += "<li>Sorry, you couldn't purchase any tickets.</li>"
     }
 
@@ -80,9 +78,7 @@ function App() {
 
       <input onClick={() => checkEligibility()} id="submit-button" type="submit" value="Check Eligibility"  />
       
-      <div>
-        {message}
-      </div>
+      <div dangerouslySetInnerHTML={{__html: message}}></div>
     </div>
   );
 }
